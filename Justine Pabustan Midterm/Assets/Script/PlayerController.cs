8b8f2592ea999d6fd667f088c8104ea4ac289d3c
RayniecoGomez <93339817+RayniecoGomez@users.noreply.github.com>
2021-10-29 01:55:02 +0800

    3DMazeMidterms

---
+++ Justine Pabustan Midterm/Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text countText;
+    private int count;
+
+
+    void Start()
+    {
+        countText.text = "";
+ }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+
+            if (count == 6)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+
+    void setCountText()
+    {
+       countText.text = "Score: " + count.ToString();
+   }
+}
---
+++ Justine Pabustan Midterm/Assets/Script/PlayerController2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController2 : MonoBehaviour
+{
+    public GameObject CompleteLevel;
+    public Text countText;
+    private int count;
+
+
+    void Start()
+    {
+        countText.text = "";
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+
+            if (count == 7)
+            {
+                CompleteLevel.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
+}
+
